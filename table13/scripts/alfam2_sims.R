d0c02492095b4bed35d9e55e3462ed9fcff311c5
Sasha D. Hafner <sasha.hafner@gmail.com>
2024-07-24 07:46:35 -0400

    reorg t13 +
    Also add "adjustment" w mediterranean temp.
    Other adjustments can be added.
    Add plot too.

---
+++ table13/scripts/alfam2_sims.R
@@ -0,0 +1,53 @@
+# ALFAM2 predictions for GD Table 13 scenarios
+
+# Load packages
+library(ALFAM2)
+library(data.table)
+library(ggplot2)
+sink('../log/R_log.txt')
+  print(sessionInfo())
+sink()
+
+# Input data
+dat <- fread('../inputs/t13_inputs.csv', skip = 2)
+
+# Adjust input data
+dat[, adjustment := 'original']
+datmed <- copy(dat)[, `:=` (adjustment = 'mediterranean', air.temp = 20)]
+dat <- rbind(dat, datmed)
+dat[, sida := paste(sid, adjustment)]
+dat[, adjustment := relevel(factor(adjustment), ref = 'original')]
+
+# ALFAM2 predictions
+# Use conf.int = 'all' to get results from all 100 parameter sets to compare to reference scenario with uncertainty
+pred <- data.table(alfam2(dat, time.incorp = 'time.incorp', conf.int = 'all', group = 'sida', pass.col = c('sid', 'adjustment')))
+# Get overall prediction with parameter set 3 also
+predm <- data.table(alfam2(dat, time.incorp = 'time.incorp', group = 'sida', pass.col = c('sid', 'adjustment')))
+
+# Compare to broadcast reference for scenario 0
+predref <- pred[sid == '0', ]
+pred <- merge(pred, predref, by = c('par.id', 'adjustment'), suffixes = c('', '.ref'))
+# red = reduction in emission as fraction applied TAN, rred = reduction as fraction of reference emission
+pred[, `:=` (red = er.ref - er, rred = 1 - er / er.ref)]
+
+predmref <- predm[sid == '0', ]
+predm <- merge(predm, predmref, by = 'adjustment', suffixes = c('', '.ref'))
+predm[, `:=` (red = er.ref - er, rred = 1 - er / er.ref)]
+
+# Quantiles in relative reduction rred
+summ <- pred[, .(lwr = quantile(rred, 0.05), med = quantile(rred, 0.5), upr = quantile(rred, 0.95)), by = .(sid, adjustment)]
+summ <- merge(summ, predm[, .(sid, adjustment, red, rred)], by = c('sid', 'adjustment'))
+
+# Plot
+ggplot(summ, aes(sid, rred, colour = adjustment, group = adjustment)) +
+  geom_point(position = position_dodge(width = 0.5)) +
+  geom_errorbar(aes(ymin = lwr, ymax = upr), width = 0, position = position_dodge(width = 0.5)) +
+  theme_bw() +
+  labs(x = 'Table 13 scenario', y = 'Emission reduction (frac. ref. emission)', colour = 'Adjustment')
+ggsave('../plots/rel_red.png', height = 4, width = 5)
+
+# Export results
+fwrite(summ, '../output/t13_reductions.csv')
+fwrite(predm, '../output/t13_preds.csv')
+
+
