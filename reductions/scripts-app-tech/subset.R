810267eb3f1dd2abb1195b39f243557c7c7227dd
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2025-01-26 12:13:12 +0100

    Update subset.R

--- reductions/scripts-app-tech/subset.R
+++ reductions/scripts-app-tech/subset.R
@@ -1,12 +1,13 @@
 
 # selecting category 1 application techniques
-df <- df[df$app.treat.general %in% c('injection', 'broadcast / incorporation', 'banding', 'Trailing shoe'), ]
+df <- df[df$app.treat.general %in% c('open slot injection', 'closed slot injection', 'broadcast / incorporation', 'banding', 'Trailing shoe', 'Banded / aeration', 'broadcast / aeration'), ]
 
-# trailing hose, trailing shoe, shallow injection, deep injection - OBS, in the above we don't distinguish between 'deep and shallow' injection
+# trailing hose, trailing shoe, shallow injection, deep injection
 
 # data frame for application techniques
 df <- df[! df$app.ref2 == df$app.treat.general , ]
 
+unique(df$additive.class)
 # selecting observations without slurry treatments (UI, acid, dilution etc.)
 df <- df[df$additive.class == 'n/a' | is.na(df$additive.class), ]
 
