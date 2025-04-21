421bbcaa5eaad6ad2b5c992ca10409f89a37ca2f
Sasha D. Hafner <sasha.hafner@bce.au.dk>
2025-04-21 19:03:32 -0400

    switch to 2-step dilution red calc.
    And add conf int calc in R

--- reductions/scripts-dilution/summ.R
+++ reductions/scripts-dilution/summ.R
@@ -1,13 +1,16 @@
 
 # calculating the average reduction pr ref 
-dfsumm <- df[, .(red.avg = mean(red), 
+summ1 <- df[, .(red = mean(red), 
                  red.sd = sd(red), 
-                 dil.fac.avg = mean(dil.fac), 
+                 dil.fac = mean(dil.fac), 
                  dil.fac.sd = sd(dil.fac)) 
                  , by = study]
 
-dfsumm <- df[, .(red.avg = mean(red), 
+dfsumm <- summ1[, .(red.avg = mean(red), 
+                 red.lwr = t.test(red)$conf.int[1],
+                 red.upr = t.test(red)$conf.int[2],
                  red.sd = sd(red), 
                  dil.fac.avg = mean(dil.fac), 
                  dil.fac.sd = sd(dil.fac), 
                  study.no = length(unique(study)))]
+
