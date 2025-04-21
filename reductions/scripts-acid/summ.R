5a40313267c7c56851ed2b7fefc86c56c8036aab
Sasha D. Hafner <sasha.hafner@bce.au.dk>
2025-04-21 19:10:09 -0400

    switch to 2-step acid red. calc.
    And add conf int in R

--- reductions/scripts-acid/summ.R
+++ reductions/scripts-acid/summ.R
@@ -4,18 +4,19 @@ df$pH.a <- as.numeric(df$pH.a)
 df$pHr <- as.numeric(df$pH.u) - as.numeric(df$pH.a)
 
 # calculating the average reduction pr ref 
-# dfsumm <- df[, .(red.avg = mean(r), 
-#                  red.sd = sd(r), 
-#                  pH.red.avg = mean(pHr), 
-#                  pH.red.sd = sd(pHr),
-#                  pH.acid.avg = mean(pH.a), 
-#                  pH.acid.sd = sd(pH.a)) 
-#                  , by = ID]
-
-dfsumm <- df[, .(red.avg = mean(r), 
+summ1 <- df[, .(red = mean(r), 
                  red.sd = sd(r), 
-                 pH.red.avg = mean(na.omit(pHr)), 
-                 pH.red.sd = sd(na.omit(pHr)),                 
-                pH.acid.avg = mean(na.omit(pH.a)), 
-                pH.acid.sd = sd(na.omit(pH.a)), 
-                 study.no = length(unique(ID)))]
+                 pHr = mean(pHr), 
+                 pHr.sd = sd(pHr),
+                 pH.a = mean(pH.a), 
+                 pH.a.sd = sd(pH.a)), by = ID]
+
+dfsumm <- summ1[, .(red.avg = mean(red), 
+                    red.sd = sd(red), 
+                    red.lwr = t.test(red)$conf.int[1],
+                    red.upr = t.test(red)$conf.int[2],
+                    pH.red.avg = mean(na.omit(pHr)), 
+                    pH.red.sd = sd(na.omit(pHr)),                 
+                    pH.acid.avg = mean(na.omit(pH.a)), 
+                    pH.acid.sd = sd(na.omit(pH.a)), 
+                    study.no = length(unique(ID)))]
