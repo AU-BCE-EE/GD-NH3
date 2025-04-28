7cd42fbb1e56b6955638c920315d51df1134d2c7
Sasha D. Hafner <sasha@hafnerconsulting.com>
2025-04-28 17:01:37 -0400

    use 95% CI, rm unused input columns, other tweaks

--- reductions-ALFAM2/log/pred_emis.md
+++ reductions-ALFAM2/log/pred_emis.md
@@ -1,7 +1,7 @@
 ---
 title: 'ALFAM2 predictions'
 output: pdf_document
-date: "21 April, 2025 Apr:04"
+date: "28 April, 2025 Apr:04"
 ---
 
 Use conf.int = 'all' to get results from all 100 parameter sets to compare to reference scenario with uncertainty
--- reductions-ALFAM2/scripts/summ.R
+++ reductions-ALFAM2/scripts/summ.R
@@ -12,9 +12,9 @@ predm <- merge(predm, predmref, by = 'man.source.pig', suffixes = c('', '.ref'))
 predm[, `:=` (red = er.ref - er, rred = 1 - er / er.ref)]
 
 # Quantiles in relative reduction rred
-summ <- pred[, .(lwr = quantile(rred, 0.05), 
+summ <- pred[, .(lwr = quantile(rred, 0.025), 
                  med = quantile(rred, 0.5), 
-                 upr = quantile(rred, 0.95)), by = .(group, man.source.pig, sid)]
+                 upr = quantile(rred, 0.975)), by = .(group, man.source.pig, sid)]
 
 summ <- merge(summ, predm[, .(group, man.source.pig, sid, rred)], by = c('group', 'man.source.pig', 'sid'))
 
