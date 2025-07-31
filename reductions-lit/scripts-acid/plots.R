f225d792d2df3293177b2a258a780145b27c3756
Sasha D. Hafner <sasha@hafnerconsulting.com>
2025-07-31 09:57:11 -0400

    add acid plot w init pH color

--- reductions-lit/scripts-acid/plots.R
+++ reductions-lit/scripts-acid/plots.R
@@ -19,6 +19,22 @@ ggplot(df, aes(pH.a, r, colour = ref)) +
   theme(legend.title = element_blank())
 ggsave2x('../plots/acid_2', height = 6, width = 8)
 
+df[, pH.u := as.numeric(pH.u)]
+ggplot(df, aes(pHr, r)) + 
+  geom_point(aes(colour = pH.u)) + 
+  scale_color_viridis(option = 'D') +  
+  scale_fill_viridis(option = 'D') +  
+  geom_point(aes(fill = pH.u), pch = 21, size = 3, colour = 'gray45', show.legend = FALSE) + 
+  geom_line(data = pred3, aes(dph, red), colour = 'gray45', lwd = 1.5) + 
+  geom_line(data = predci, aes(dph, red.lwr), lty = 2, colour = 'gray55') + 
+  geom_line(data = predci, aes(dph, red.upr), lty = 2, colour = 'gray55') + 
+  theme_bw() + 
+  theme(legend.position = 'top') +
+  xlab('Change in pH due to acidification') + ylab('Reduction due to acidification (%)') +
+  labs(colour = 'Untreated slurry pH') 
+ggsave2x('../plots/acid_3', height = 6, width = 8)
+
+
 # What needs to be done: 
 # Combining figures with one legend. 
 # For ALFAM2 predictions, decide: Which inputs to use? What duration? Especially for slurry....? 
