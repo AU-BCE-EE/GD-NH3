a819328bf386c9627ab22f4b2c252c54e9dce5d6
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2025-11-01 18:30:33 +0100

    Update summ.R

--- chapter_7/reductions-lit/scripts-dilution/summ.R
+++ chapter_7/reductions-lit/scripts-dilution/summ.R
@@ -1,9 +1,17 @@
 
+df$DMr <- df$ref.dm - df$dil.dm
+
 # calculating the average reduction pr ref 
 summ1 <- df[, .(red = mean(red), 
                  red.sd = sd(red), 
                  dil.fac = mean(dil.fac), 
-                 dil.fac.sd = sd(dil.fac)) 
+                 dil.fac.sd = sd(dil.fac), 
+                ref.dm = mean(ref.dm), 
+                ref.dm.sd = sd(ref.dm), 
+                dil.dm = mean(dil.dm), 
+                dil.dm.sd = mean(dil.dm), 
+                DM.r = mean(DMr), 
+                DM.r.sd = mean(DMr)) 
                  , by = study]
 
 dfsumm <- summ1[, .(red.avg = mean(red), 
@@ -12,10 +20,11 @@ dfsumm <- summ1[, .(red.avg = mean(red),
                  red.sd = sd(red), 
                  dil.fac.avg = mean(dil.fac), 
                  dil.fac.sd = sd(dil.fac), 
+                 ref.dm.avg = mean(ref.dm), 
+                 dil.dm.avg = mean(dil.dm), 
+                 DM.r.avg = mean(DM.r),
                  study.no = length(unique(study)))]
 
+
+# Count application methods
 appsumm <- table(df$app.meth)
-
-# calculating the average DM reduction (% of reference/undiluted DM)
-
-
