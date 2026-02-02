a71b31a1ef5c4d3951b08b6b848c1d30793a6eb9
Sasha D. Hafner <sasha@hafnerconsulting.com>
2026-02-02 16:30:07 -0500

    add () to acid plot ref dates

--- chapter_7/reductions-lit/scripts-acid/clean.R
+++ chapter_7/reductions-lit/scripts-acid/clean.R
@@ -10,3 +10,11 @@ df1 <- df1[! df1$app.treat.general %in% c('irrigation', 'stubble coverage'), ]
 
 # removing rows with 'NA' in red2 (reduction)
 df1 <- df1[! df1$red.2 == 'N/A', ]
+
+# Add () to dates
+df1 <- df1[, study := gsub(' ([12])', ' (\\1', study)]
+df1 <- df1[, study := gsub('([0-9ab])$', '\\1)', study)]
+
+df2 <- df2[, ref := gsub(' ([12])', ' (\\1', ref)]
+df2 <- df2[, ref := gsub('([0-9ab])$', '\\1)', ref)]
+
--- chapter_7/reductions-lit/scripts-acid/load.R
+++ chapter_7/reductions-lit/scripts-acid/load.R
@@ -1,8 +1,6 @@
 
-org1 <- read_excel('../data/lit_dat.xlsx', 2)
-df1 <- org1
-df1 <- as.data.table(df1)
+df1 <- read_excel('../data/lit_dat.xlsx', 2)
+setDT(df1)
 
-org2 <- read_excel('../data/acidification_dat_extra.xlsx', 1)
-df2 <- org2
-df2 <- as.data.table(df2)
+df2 <- read_excel('../data/acidification_dat_extra.xlsx', 1)
+setDT(df2)
